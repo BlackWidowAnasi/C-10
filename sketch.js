2c44f9977ba0829e3d03ff9a8cb938b595576380
BlackWidowAnasi <87397602+BlackWidowAnasi@users.noreply.github.com>
2021-07-20 15:24:47 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+var weight = [38, 40, 42, 44];
+var sum = weight[0] + weight[1] + weight[2] + weight[3]
+
+var averageWeight = sum/weight.length
+
+console.log (averageWeight)
+
+function setup() 
+{
+  createCanvas(400,400);
+}
+
+function draw() 
+{
+background(51);
+
+}
+
